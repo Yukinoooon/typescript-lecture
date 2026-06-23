83bd55141cbfe60edfebaaf2ec1004a70cc6dc42
kn-koko <r.k.v.1225kaito@gmail.com>
2026-06-23 15:27:35 +0900

    lecture 5 課題 継承・抽象クラス

---
+++ src/enemy.ts
@@ -0,0 +1,31 @@
+import { Character } from "./character";
+
+export class Enemy extends Character {
+  readonly maxHp: number;
+
+  constructor(name: string, hp: number) {
+    super(name, hp);
+    this.maxHp = hp;
+  }
+
+  // maxHp に対する現在の hp の割合を返す
+  getHpRatio(): number {
+    return this.hp / this.maxHp;
+  }
+
+  // ダメージを受ける takeDamage メソッドを追加する
+  takeDamage(damage: number): void {
+    this.hp -= damage;
+  }
+
+  // 通常時 : name + "は攻撃してきた！"
+  // 残り HP が 30% 以下 : name + "は必死に抵抗している！"
+  attack(): void {
+    if (this.getHpRatio() > 0.3) {
+      console.log(`${this.name}は攻撃してきた！`);
+    } else {
+      console.log(`${this.name}は必死に抵抗している！`);
+    }
+
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import { Mage } from "./mage";
-import { Warrior } from "./warrior";
+import { Enemy } from "./enemy";
 
-// const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
-
-const warrior = new Warrior("アーサー", 100, "エクスカリバー");
-warrior.showStatus();
-warrior.attack();
-
-const mage = new Mage("メディア", 80);
-mage.showStatus();
-mage.attack();
+const slime = new Enemy("スライム", 50);
+slime.showStatus();
+slime.attack();
+slime.takeDamage(40);
+slime.showStatus();
+slime.attack();
