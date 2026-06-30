73d3634fed967ae83baf187c704787c61c4887cb
Yukino <yukimo.no.222@gmail.com>
2026-06-30 14:09:50 +0900

    追記・編集済み

--- src/characters/enemy.ts
+++ src/characters/enemy.ts
@@ -1,14 +1,16 @@
 import { Character } from "./character";
 
 export class Enemy extends Character {
+  //maxHp プロパティを追加する
   readonly maxHp: number;
 
+  //maxHp プロパティの初期化
   constructor(name: string, hp: number) {
     super(name, hp);
     this.maxHp = hp;
   }
 
-  // maxHp に対する現在の hp の割合を返す
+  // maxHp に対する現在の hp の割合を返すgetHpRatio メソッドを追加する
   getHpRatio(): number {
     return this.hp / this.maxHp;
   }
@@ -18,8 +20,8 @@ export class Enemy extends Character {
     this.hp -= damage;
   }
 
-  // 通常時 : name + "は攻撃してきた！"
-  // 残り HP が 30% 以下 : name + "は必死に抵抗している！"
+  // 通常時: name + "は攻撃してきた！"
+  // 残HPが30%以下: name + "は必死に抵抗している！"
   attack(): void {
     if (this.getHpRatio() > 0.3) {
       console.log(`${this.name}は攻撃してきた！`);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { Enemy } from "./characters/enemy";
 
 const slime = new Enemy("スライム", 50);
+slime.showStatus();
 slime.attack();
 slime.takeDamage(40);
 slime.showStatus();
 slime.attack();
-
-console.log("これはpushテスト用のログです");
